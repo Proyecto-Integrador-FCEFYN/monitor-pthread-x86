02a426783084b5c2b15a10b4068774a91d5425aa
Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>
2021-10-20 20:54:22 -0300

    Se agregan los 'objetos' monitor y vcondicion.
    Se agregan las funciones de init para monitor y vcondicion.
    Se mejora el scope de las funciones anteriores.
    
    Signed-off-by: Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>

--- monitor.c
+++ monitor.c
@@ -1,21 +1,70 @@
 #include "monitor.h"
 
-void monitor()
+
+int monitor_get_bloqueados(monitor_t *monitor, int d)
 {
-    pthread_mutex_init(&entrada, NULL);
-    //red de petri
-    //politica()
-    //variables de condicion
+    return monitor->var_condicion[d].bloqueados;
 }
 
-void monitor_disparar(int disparo)
+void monitor_disparar(monitor_t *monitor, int disparo)
 {
-    pthread_mutex_lock(&entrada);
+    //try
+    pthread_mutex_lock(&monitor->entrada);
+    while ( !monitor->petri->solicitud_disparo(monitor->petri, disparo) )
+    {
+
+    }
 
     //mucho codigo
 
-    pthread_mutex_unlock(&entrada);
+    pthread_mutex_unlock(&monitor->entrada);
 }
+
+void monitor_init(monitor_t *monitor, procesador_petri_t *petri)
+{
+    pthread_mutex_init(&monitor->entrada, NULL);
+    politica_init(&monitor->politica);
+    for (int i = 0; i < TRANSICIONES; ++i)
+    {
+        vcondicion_init(&monitor->var_condicion[i], &monitor->entrada);
+        monitor->politica.add_element(&monitor->politica,i);
+    }
+    monitor->petri = petri;
+    monitor->disparar = monitor_disparar;
+    monitor->get_bloqueados = monitor_get_bloqueados;
+}
+
+void vcondicion_delay(variable_condicion_t *condicion)
+{
+    condicion->bloqueados++;
+    pthread_mutex_unlock(condicion->mutex);
+
+    pthread_cond_wait(&condicion->var_condicion, condicion->mutex);
+    // ver manejo de errores.
+}
+void vcondicion_resume(variable_condicion_t *condicion)
+{
+    if (condicion->bloqueados>0)
+    {
+        condicion->bloqueados--;
+        pthread_cond_signal(&condicion->var_condicion);
+    }
+}
+int vcondicion_is_empty(variable_condicion_t *condicion)
+{
+    return condicion->bloqueados==0;
+}
+
+void vcondicion_init(variable_condicion_t *condicion, pthread_mutex_t *mutex)
+{
+    condicion->bloqueados = 0;
+    condicion->mutex = mutex;
+    condicion->delay = vcondicion_delay;
+    condicion->resume = vcondicion_resume;
+    condicion->is_empty = vcondicion_is_empty;
+}
+
+
 
 
 
--- monitor.h
+++ monitor.h
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <pthread.h>
-#define TRANSICIONES 8
+#include "procesador_petri.h"
+#include "politica.h"
+#define TRANSICIONES 6
 
+typedef struct monitor_t monitor_t;
+typedef struct variable_condicion_t variable_condicion_t;
 
-typedef struct monitor
+struct variable_condicion_t {
+    pthread_cond_t var_condicion;
+    pthread_mutex_t *mutex;
+    int bloqueados;
+    void (*delay)(variable_condicion_t *condicion);
+    void (*resume)(variable_condicion_t *condicion);
+    int (*is_empty)(variable_condicion_t *condicion);
+};
+
+struct monitor_t
 {
     pthread_mutex_t entrada;
-    pthread_cond_t var_condicion[TRANSICIONES];
-    int *red_petri;
-} monitor_t;
+    variable_condicion_t var_condicion[TRANSICIONES];
+    procesador_petri_t *petri;
+    politica_t politica;
+    void (*disparar)(monitor_t *monitor, int disparo);
+    int (*get_bloqueados)(monitor_t *monitor, int d);
+};
 
 
-void monitor_init(monitor_t *monitor);
+void monitor_init(monitor_t *monitor, procesador_petri_t *petri);
 
-void monitor_disparar(monitor_t *monitor, int disparo);
+void vcondicion_init(variable_condicion_t *condicion, pthread_mutex_t *mutex);
 
-int monitor_get_bloqueados(monitor_t *monitor, int d);
