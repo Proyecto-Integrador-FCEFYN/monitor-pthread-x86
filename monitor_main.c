e1f93ef7ea1f95493fd48d177adaa97a571e7765
Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>
2021-10-21 01:00:19 -0300

    Primer ejemplo del monitor disparando una transicion.
    Signed-off-by: Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>

--- monitor_main.c
+++ monitor_main.c
@@ -1,9 +1,6 @@
 
-#include <stdio.h>
-#include "politica.h"
-//#include "procesador_petri.h"
-//#include "lector_de_matriz.h"
 #include "procesador_petri.h"
+#include "monitor.h"
 
 /// Esta funcion por ahora la usemos para probar los distintos metodos que
 /// vayamos implementando.
@@ -13,21 +10,21 @@ int main()
 {
     // PRUEBAS DE POLITICA
 
-    politica_t politica;
-    politica_init(&politica);
+//    politica_t politica;
+//    politica_init(&politica);
 
-    politica.add_element(&politica,0);
-    politica.add_element(&politica,1);
-    politica.add_element(&politica,2);
-    politica.add_element(&politica,3);
-    politica.add_element(&politica,4);
-    politica.add_element(&politica,5);
-    politica.add_element(&politica,6);
-    politica.add_element(&politica,7);
-
-    politica.modificar_prioridad(&politica,3);
-
-    printf("%i",politica.get_numero_elementos(&politica));
+//    politica.add_element(&politica,0);
+//    politica.add_element(&politica,1);
+//    politica.add_element(&politica,2);
+//    politica.add_element(&politica,3);
+//    politica.add_element(&politica,4);
+//    politica.add_element(&politica,5);
+//    politica.add_element(&politica,6);
+//    politica.add_element(&politica,7);
+//
+//    politica.modificar_prioridad(&politica,3);
+//
+//    printf("%i",politica.get_numero_elementos(&politica));
 
     // PRUEBAS DE LECTOR DE MATRIZ
 
@@ -38,9 +35,30 @@ int main()
 //                          "../redes-de-petri/productor-consumidor/marcado-prod-cons.csv");
 
 //    lector.leer(&lector);
-procesador_petri_t petri;
+    procesador_petri_t petri;
     procesador_de_petri_init(&petri);
 
+    monitor_t monitor;
+    monitor_init(&monitor, &petri);
+
+    monitor.disparar(&monitor, 0);
+//    int i;
+//    pthread_attr_t atrib;
+//    pthread_t c[2];
+//    pthread_attr_init(&atrib) ;
+//    pthread_attr_setscope(&atrib, PTHREAD_SCOPE_SYSTEM);
+//    i=pthread_attr_setschedpolicy(&atrib, SCHED_RR);
+//    printf("%d\n", i);
+//
+//    for (i=0;i<2;i++)
+//    {
+//        pthread_create(&c[i], &atrib,  monitor.disparar, (void*) i);
+//    }
+//
+//    for (i=0;i<2;i++)
+//    {
+//        pthread_join(c[i], NULL);
+//    }
 
 
 
