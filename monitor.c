2a44691c02bd9f4732c910b703557b9605d5c614
Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>
2021-10-24 23:59:13 -0300

    se quitan funciones y parametros innecesarios
    Signed-off-by: Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>

--- monitor.c
+++ monitor.c
@@ -16,11 +16,6 @@ static int log(int disparo)
     return 1;
 }
 
-int monitor_get_bloqueados(monitor_t *monitor, int d)
-{
-    return monitor->var_condicion[d].bloqueados;
-}
-
 void monitor_disparar2(monitor_t *monitor, int disparo)
 {
     pthread_mutex_lock(&monitor->entrada);
@@ -54,41 +49,12 @@ void monitor_disparar2(monitor_t *monitor, int disparo)
 void monitor_init(monitor_t *monitor, procesador_petri_t *petri)
 {
     pthread_mutex_init(&monitor->entrada, NULL);
-    politica_init(&monitor->politica);
     for (int i = 0; i < TRANSICIONES; ++i)
     {
         pthread_cond_init(&monitor->condition[i], NULL);
-        vcondicion_init(&monitor->var_condicion[i], &monitor->entrada);
-        monitor->politica.add_element(&monitor->politica,i);
     }
     monitor->petri = petri;
     monitor->disparar = monitor_disparar2;
-    monitor->get_bloqueados = monitor_get_bloqueados;
 }
 
-void vcondicion_delay(variable_condicion_t *condicion)
-{
-    condicion->bloqueados++;
-    pthread_cond_wait(&condicion->var_condicion, condicion->mutex);
-}
-void vcondicion_resume(variable_condicion_t *condicion)
-{
-    if (condicion->bloqueados>0)
-    {
-        condicion->bloqueados--;
-        pthread_cond_signal(&condicion->var_condicion);
-    }
-}
-int vcondicion_is_empty(variable_condicion_t *condicion)
-{
-    return condicion->bloqueados==0;
-}
 
-void vcondicion_init(variable_condicion_t *condicion, pthread_mutex_t *mutex)
-{
-    condicion->bloqueados = 0;
-    condicion->mutex = mutex;
-    condicion->delay = vcondicion_delay;
-    condicion->resume = vcondicion_resume;
-    condicion->is_empty = vcondicion_is_empty;
-}
--- monitor.h
+++ monitor.h
@@ -2,33 +2,19 @@
 #include <pthread.h>
 #include "procesador_petri.h"
 #include "politica.h"
-#define TRANSICIONES 6
+#include "macros.h"
 
 typedef struct monitor_t monitor_t;
-typedef struct variable_condicion_t variable_condicion_t;
-
-struct variable_condicion_t {
-    pthread_cond_t var_condicion;
-    pthread_mutex_t *mutex;
-    int bloqueados;
-    void (*delay)(variable_condicion_t *condicion);
-    void (*resume)(variable_condicion_t *condicion);
-    int (*is_empty)(variable_condicion_t *condicion);
-};
 
 struct monitor_t
 {
     pthread_mutex_t entrada;
-    variable_condicion_t var_condicion[TRANSICIONES];
     pthread_cond_t condition[TRANSICIONES];
     procesador_petri_t *petri;
-    politica_t politica;
     void (*disparar)(monitor_t *monitor, int disparo);
-    int (*get_bloqueados)(monitor_t *monitor, int d);
 };
 
 
 void monitor_init(monitor_t *monitor, procesador_petri_t *petri);
 
-void vcondicion_init(variable_condicion_t *condicion, pthread_mutex_t *mutex);
 
